fdb35bd2f7d9f0c54a7baf9c6158f0927facf743
Lele <wbnominat@bk.ru>
2024-06-17 23:19:28 +0300

    Add files via upload

---
+++ homework2.1.py
@@ -0,0 +1,5 @@
+the_number_of_completed_tasks = 12
+the_number_of_hours_spend = 1.5
+cours_name = "Pythone"
+time_for_one_task = (12 / 1.5)
+print ("cours_name", "the_number_of_completed_tasks", "the_number_of_hours_spend", "time_for_one_task", "0.125 часа")
